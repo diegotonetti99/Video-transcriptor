ff5af6ee96cf6ff4d3bfb605764f0e9ea73ef3cc
diegotonetti99 <76702139+diegotonetti99@users.noreply.github.com>
2020-12-28 19:42:05 +0100

    Add files via upload

--- transcriber.py
+++ transcriber.py
@@ -3,6 +3,11 @@ import os
 import moviepy.editor as mp
 from pydub import AudioSegment
 
+
+#Define transciption language
+LANGUAGE = 'it-IT'
+SILECE_THRESHOLD = '-30' #value in dB, lower noises are considered as silence
+
 #load mp4 ad extract wav track
 print('Loading video...')
 files = os.listdir()
@@ -12,6 +17,11 @@ for f in files:
         video_name = f
         break
 
+if not video_name:
+    print('No video found')
+    exit()
+else:
+    print('{} found'.format(video_name))
 video = mp.VideoFileClip(video_name)
 audio_file_name = 'track.wav'
 print('Extracting audio...')
@@ -20,7 +30,8 @@ video.audio.write_audiofile(audio_file_name)
 
 #split audio file
 print('Splitting audio...')
-command = 'sh ST.sh '+ audio_file_name +' -30 1'
+#Create silence.txt file where are contained all the start and end seconds of all the silences
+command = 'sh ST.sh '+ audio_file_name + ' '+ SILECE_THRESHOLD +' 1' #-30 is the dB silence threshold, 1 is the minimum silence duration
 os.system(command)
 silences = open('silence.txt', 'r').readlines()
 audio_file = AudioSegment.from_mp3(audio_file_name)
@@ -29,11 +40,13 @@ tracks = []
 for line in silences:
     end,duration = line.strip().split()
     try:
+        #library works in ms so it's all multiplied by 1000
         to = (float(end) - float(duration)) * 1000
         start = float(last) * 1000
         clip_len = to - start
         #if clip last less than 5 sec it doesn't split
-        if clip_len > 5000:
+        if clip_len > 5 * 1000:
+            #extract an audio segment from the source
             tracks.append(audio_file[start:to])
             last = end
     except:
@@ -47,14 +60,17 @@ print('Starting transcription...')
 text = ''
 r = sr.Recognizer()
 for track in tracks:
+    #Create a temp file from the audio segment
     f = 'temp.wav'
     track.export(f, format='wav')
     with sr.AudioFile(f) as source:
         audio = r.record(source)
+        #Gets transcription of the audio segment
         try:
-            r_text = r.recognize_google(audio, language="it-IT")
+            r_text = r.recognize_google(audio, language=LANGUAGE)
         except:
             r_text = 'ERROR'
+        #Append a new transcribed line of text to the transcription
         text +='\n' + r_text
         name = video_name.replace('.mp4', '.txt')
         text_file = open(name, 'w+')
